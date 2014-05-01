a31bad79660b8fab7df1b8dd7eda7752dc222e17
Arthur Puyou <arthur@puyou.fr>
2014-05-01 02:20:44 +0200

    Uppercase for UID

--- src/fr/utc/assos/payutc/CardReader.java
+++ src/fr/utc/assos/payutc/CardReader.java
@@ -134,9 +134,9 @@ public class CardReader {
 	private String getHexString(byte[] b) {
 		String result = "";
 		for (int i=0; i < b.length; i++) {
-			result += Integer.toString( ( b[i] & 0xff ) + 0x100, 16).substring( 1 );
+			result += Integer.toString((b[i] & 0xff) + 0x100, 16).substring(1);
 		}
-		return result;
+		return result.toUpperCase();
 	}
 }
 
