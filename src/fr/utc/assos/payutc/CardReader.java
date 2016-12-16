7e19890578c05f96a74d1196cee3f1409d3b35bc
Sylvain M <sylvain.marchienne@gmail.com>
2016-12-16 15:47:17 +0100

    Indication si la badgeuse est introuvable plutôt que la simulation d'une fausse badgeuse

--- src/fr/utc/assos/payutc/CardReader.java
+++ src/fr/utc/assos/payutc/CardReader.java
@@ -16,7 +16,7 @@ import javax.smartcardio.TerminalFactory;
 public class CardReader {
 	private CardTerminal mReader;
 	private EventSocket mSocket;
-	
+
 	private byte[] mApduArray = {
 			(byte) 0xFF,
 			(byte) 0xCA,
@@ -24,76 +24,80 @@ public class CardReader {
 			(byte) 0x00,
 			(byte) 0x00
 	};
-	
+
 	private CardThread mThread;
 
 	public CardReader(EventSocket socket) {
 		try {
 			// Store pointer to socket
 			mSocket = socket;
-			
+
 			// Get the list of readers
 			TerminalFactory factory = TerminalFactory.getDefault();
 			List<CardTerminal> terminals = factory.terminals().list();
 	        System.out.println("Liste des lecteurs détectés : " + terminals);
-	        
+
 			// Choose the first reader
-	        if(terminals.size() > 0){
-	        	mReader = terminals.get(0);
-	        	System.out.println("Connecté au premier lecteur.");
-	        	
-	        	// Start loop in a thread
-	        	mThread = new CardThread();
-	        	new Thread(mThread).start();
-	        }
-	        else {
-	        	System.out.println("Aucun lecteur détecté, passage en mode simulation");
-	        	mThread = new FakeCardThread();
-	        	new Thread(mThread).start();
-	        }
+      if(terminals.size() > 0){
+      	mReader = terminals.get(0);
+      	System.out.println("Connecté au premier lecteur.");
+
+      	// Start loop in a thread
+      	mThread = new CardThread();
+      	new Thread(mThread).start();
+      }
+      else {
+				/*
+      	System.out.println("Aucun lecteur détecté, passage en mode simulation");
+      	mThread = new FakeCardThread();
+      	new Thread(mThread).start();
+				*/
+				System.out.println("Aucun lecteur détecté, on envoi un message d'erreur badgeuseNotFound");
+				mSocket.sendData("badgeuseNotFound: 1");
+      }
+
 		}  catch (CardException e) {
-			System.out.println("Impossible de récupérer la liste de lecteurs, passage en mode simulation");
-        	mThread = new FakeCardThread();
-        	new Thread(mThread).start();
+			System.out.println("Impossible de récupérer la liste de lecteurs, on envoi un message d'erreur badgeuseNotFound");
+			mSocket.sendData("badgeuseNotFound: 1");
 		}
 	}
-	
+
 	public void close() {
 		// Tell the loop to stop
 		mThread.stop();
 	}
-	
+
 	private class CardThread implements Runnable {
 		private volatile boolean mRun = true;
-		
+
 		public void stop() {
 			mRun = false;
 		}
-		
+
 		public void run() {
 			CommandAPDU GetData = new CommandAPDU(mApduArray);
 			while(mRun){
 				try {
 					if(mReader.waitForCardPresent(500)) {
 						System.out.println("Carte détectée.");
-						
+
 						// Connect to the card currently in the reader
 						Card card = mReader.connect("*");
-						
+
 						// Exchange APDUs with the card
 						CardChannel channel = card.getBasicChannel();
 						ResponseAPDU CardApduResponse = channel.transmit(GetData);
-						
+
 						// Disconnect
 						card.disconnect(true);
-						
+
 						System.out.println("Carte lue");
-						
+
 						String carduid = getHexString(CardApduResponse.getData());
-						
+
 						System.out.println("cardInserted:" + carduid);
 						mSocket.sendData("cardInserted:" + carduid);
-						
+
 						mReader.waitForCardAbsent(0);
 					}
 				} catch (CardException e) {
@@ -102,15 +106,15 @@ public class CardReader {
 			}
 		}
 	}
-	
+
 	private class FakeCardThread extends CardThread {
 		private volatile boolean mRun = true;
-		
+
 		@Override
 		public void stop() {
 			mRun = false;
 		}
-		
+
 		@Override
 		public void run() {
 			try {
@@ -121,7 +125,7 @@ public class CardReader {
 					buffer.readLine();
 					if(mSocket.isConnected()) {
 						System.out.println("cardInserted:" + cardId.toString());
-						mSocket.sendData("cardInserted:" + cardId.toString());	
+						mSocket.sendData("cardInserted:" + cardId.toString());
 					}
 				}
 			} catch (IOException e) {
@@ -130,7 +134,7 @@ public class CardReader {
 			}
 		}
 	}
-	
+
 	private String getHexString(byte[] b) {
 		String result = "";
 		for (int i=0; i < b.length; i++) {
@@ -139,4 +143,3 @@ public class CardReader {
 		return result.toUpperCase();
 	}
 }
-
